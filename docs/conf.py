7daccaadec3521af85fc0aa75be4e764eed6d182
Dermasugita <itsukiisogin@gmail.com>
2020-10-24 16:10:24 +0900

    add path to the favicon

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_favicon='_static/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

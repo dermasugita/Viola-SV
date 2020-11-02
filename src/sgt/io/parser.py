cd0340ab5a9d828cf0c7ee3565fd129ac9282455
Dermasugita <itsukiisogin@gmail.com>
2020-11-03 00:22:56 +0900

    fixed typo

--- src/sgt/io/parser.py
+++ src/sgt/io/parser.py
@@ -34,7 +34,7 @@ def read_vcf(filepath_or_buffer: Union[str, StringIO], variant_caller: str = "ma
     """
     # read vcf files using PyVcf package
     if isinstance(filepath_or_buffer, str) and is_url(filepath_or_buffer):
-        vcf_reader = vcf.Reader(filepath_ro_buffer)
+        vcf_reader = vcf.Reader(filepath_or_buffer)
     elif isinstance(filepath_or_buffer, str):
         vcf_reader = vcf.Reader(open(filepath_or_buffer, 'r'))
     elif isinstance(filepath_or_buffer, StringIO):

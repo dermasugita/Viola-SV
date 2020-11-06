d04784b32353a28a2ecd23f15e6ee1c82eef086b
gpatsugita <itsukiisogin@gmail.com>
2020-11-06 15:09:57 +0900

    add: read_bed

--- src/sgt/__init__.py
+++ src/sgt/__init__.py
@@ -1,6 +1,6 @@
 import pandas
 import vcf
-from sgt.io.parser import read_vcf, read_bedpe
+from sgt.io.parser import read_vcf, read_bedpe, read_bed
 from sgt.core.db import Vcf, Bedpe
 import sgt.testing
 import sgt._typing
---
+++ src/sgt/core/bed.py
@@ -0,0 +1,34 @@
+from intervaltree import Interval, IntervalTree
+import pandas as pd
+class Bed(object):
+    
+    def __init__(self, df, header):
+        self._df = df
+        self._header = header
+        self._tree = self._intervaltree_init(df)
+    
+    def _intervaltree_init(self, df):
+        dict_tree = dict()
+        for idx, row in df.iterrows():
+            chrom = row['chrom']
+            st = row['chromStart']
+            en = row['chromEnd']
+            if dict_tree.get(chrom) is None:
+                dict_tree[chrom] = IntervalTree()
+            dict_tree[chrom][st:en] = row
+        
+        return dict_tree
+    
+    def query(self, chrom, st, en=None) -> pd.DataFrame:
+        if self._tree.get(chrom) is None:
+            return pd.DataFrame(columns = self._df.columns)
+        tree = self._tree[chrom]
+        if en is None:
+            set_out = tree[st]
+        else:
+            set_out = tree[st:en]
+        if len(set_out) == 0:
+            return pd.DataFrame(columns = self._df.columns)
+        ls_out = list(set_out)
+        ls_ser_out = [x.data for x in ls_out]
+        return pd.DataFrame(ls_ser_out)

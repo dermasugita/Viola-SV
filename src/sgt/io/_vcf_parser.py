19d4a96a339f0c17e3c7243b54c2ec7b8eaa8044
gpatsugita <itsukiisogin@gmail.com>
2021-02-27 12:34:47 +0900

    start coding delly support

---
+++ src/sgt/io/_vcf_parser.py
@@ -0,0 +1,401 @@
+import vcf
+import pandas as pd
+from collections import OrderedDict
+from sgt.core.db import Vcf
+
+################################################
+# Manta
+################################################
+
+def read_vcf_manta(vcf_reader):
+    # obtain header informations
+    odict_contigs = vcf_reader.contigs
+    df_contigs_meta = pd.DataFrame(odict_contigs, index=('id', 'length')).T.reset_index(drop=True)
+
+    # obtain alteration class informations
+    odict_alts = vcf_reader.alts
+    df_alts_meta = pd.DataFrame(odict_alts, index=('id', 'description')).T.reset_index(drop=True)
+
+    # obtain info field information
+    odict_infos = vcf_reader.infos
+    df_infos_meta = pd.DataFrame(odict_infos, index=('id', 'number', 'type', 'description', 'source', 'version')).T.reset_index(drop=True)
+
+    # obtain FORMAT informations
+    odict_formats = vcf_reader.formats
+    df_formats_meta = pd.DataFrame(odict_formats, index=('id', 'number', 'type','description')).T.reset_index(drop=True)
+
+    # obtain FILTER informations
+    odict_filters = vcf_reader.filters
+    df_filters_meta = pd.DataFrame(odict_filters, index=('id', 'description')).T.reset_index(drop=True)
+
+    # obtain SAMPLE informations
+    ls_samples = vcf_reader.samples
+    df_samples = pd.DataFrame(ls_samples, columns=['id'])
+
+    odict_df_headers = OrderedDict(
+        contigs_meta = df_contigs_meta,
+        alts_meta = df_alts_meta, 
+        infos_meta = df_infos_meta, 
+        formats_meta = df_formats_meta, 
+        filters_meta = df_filters_meta, 
+        samples_meta = df_samples)
+
+    
+    ls_pos = []
+    ls_filters = []
+    dict_infos = {k: [] for k in df_infos_meta.id}
+    ls_df_formats = []
+
+    
+    for record in vcf_reader:
+        dict_row = {}
+        # ex: Record(CHROM=chr1, POS=9163435, REF=G, ALT=[<DUP:TANDEM>])
+        row_ID = record.ID
+        row_CHROM1 = record.CHROM
+        row_POS1 = record.POS
+        row_REF = record.REF
+        row_ALT = record.ALT[0] # This operation is safe when parsing manta vcf files, but potential cause of information loss in another callers.
+        row_QUAL = record.QUAL
+         
+        
+        #####FILTER
+        ls_filter = record.FILTER
+        if ls_filter is None:
+            ls_filter = ['PASS']
+        elif len(ls_filter) == 0:
+            ls_filter = ['PASS'] 
+        row_FILTER = ls_filter
+        for filter_ in row_FILTER:
+            ls_filters.append({'id': row_ID, 'filter': filter_})
+        #####/FILTER 
+
+        #####INFO
+        row_INFO = record.INFO
+        row_SVTYPE = row_INFO['SVTYPE']
+        for info in df_infos_meta.id:
+            values = row_INFO.get(info, 'none')
+            if values == 'none':
+                continue
+            if not isinstance(values, list):
+                values = [values]
+            ls_keys = ['id', 'value_idx', info.lower()] 
+            for idx, value in enumerate(values):
+                ls_value = [row_ID, idx, value]
+                dict_a_info = {k: v for k, v in zip(ls_keys, ls_value)}
+                dict_infos[info].append(dict_a_info)
+        #####/INFO
+
+        ###POS
+        if isinstance(row_ALT, vcf.model._Breakend):
+            row_CHROM2 = row_ALT.chr
+            row_POS2 = row_ALT.pos
+            row_STRAND1 = '-' if row_ALT.orientation else '+'
+            row_STRAND2 = '-' if row_ALT.remoteOrientation else '+'
+        elif row_SVTYPE == 'INV': # manta-specific operation
+            row_CHROM2 = row_CHROM1
+            row_POS2 = row_INFO['END']
+            # Manta
+            if row_INFO.get('INV3', False):
+                row_STRANDs = '+'
+            else:
+                row_POS1 += 1
+                row_POS2 += 1
+                row_STRANDs = '-'
+            # /Manta
+            row_STRAND1, row_STRAND2 = row_STRANDs, row_STRANDs
+        elif row_SVTYPE == 'DEL':
+            row_CHROM2 = row_CHROM1
+            row_POS2 = row_INFO['END'] + 1
+            row_STRAND1 = '+'
+            row_STRAND2 = '-'
+        elif row_SVTYPE == 'DUP':
+            row_CHROM2 = row_CHROM1
+            row_POS2 = row_INFO['END']
+            row_POS1 += 1
+            row_STRAND1 = '-'
+            row_STRAND2 = '+'
+        else:
+            row_CHROM2 = row_CHROM1
+            row_POS2 = row_INFO['END']
+            row_STRAND1 = '.'
+            row_STRAND2 = '.'
+
+        row_SVTYPE = row_SVTYPE
+        ls_pos.append({
+                'id': row_ID, 
+                'chrom1': row_CHROM1, 
+                'pos1': row_POS1, 
+                'chrom2': row_CHROM2, 
+                'pos2': row_POS2, 
+                'strand1': row_STRAND1, 
+                'strand2': row_STRAND2,
+                'ref': row_REF,
+                'alt': row_ALT,
+                'qual': row_QUAL,
+                'svtype': row_SVTYPE
+            })
+        ###/POS
+
+        ####FORMAT
+        format_ = record.FORMAT
+
+        # operation below is limited for manta
+        ls_formats = []
+        for a_sample in record.samples:
+            for a_format in format_.split(':'):
+                values = eval('a_sample.data.' + str(a_format))
+                if not isinstance(values, list):
+                    values = [values]
+                for value_idx in range(len(values)):
+                    ls_formats.append([row_ID, a_sample.sample, a_format, value_idx, values[value_idx]])
+        df_formats_each_record = pd.DataFrame(ls_formats)
+        ls_df_formats.append(df_formats_each_record)
+        # end of the manta-limited operation
+
+        ####/FORMAT
+
+    ###FILTER
+    df_filters = pd.DataFrame(ls_filters)
+    ###/FILTER
+    
+    ###INFO
+    ls_df_infos = []
+    for info in df_infos_meta.id:
+        if len(dict_infos[info]) == 0:
+            df_a_info = pd.DataFrame(columns=('id', 'value_idx', info.lower()))
+        else:
+            df_a_info = pd.DataFrame(dict_infos[info])
+        ls_df_infos.append(df_a_info)
+    odict_df_infos = OrderedDict([(k, v) for k, v in zip(df_infos_meta.id, ls_df_infos)])
+    ###/INFO
+
+    ###POS
+    df_pos = pd.DataFrame(ls_pos)
+    df_pos = df_pos[['id', 'chrom1', 'pos1', 'chrom2', 'pos2', 'strand1', 'strand2', 'ref', 'alt', 'qual', 'svtype']]
+    ###/POS
+
+    ###FORMAT
+    df_formats = pd.concat(ls_df_formats, ignore_index=True)
+    columns = ['id', 'sample', 'format', 'value_idx', 'value']
+    df_formats.columns = columns
+    ###/FORMAT
+   
+    args = [df_pos, df_filters, odict_df_infos, df_formats, odict_df_headers]
+    return Vcf(*args)
+
+################################################
+# /Manta
+################################################
+
+################################################
+# Delly
+################################################
+
+def read_vcf_delly(vcf_reader):
+    """
+    Replace table name SVLEN into SVLENORG to avoid name conflict.
+    """
+    # obtain header informations
+    odict_contigs = vcf_reader.contigs
+    df_contigs_meta = pd.DataFrame(odict_contigs, index=('id', 'length')).T.reset_index(drop=True)
+
+    # obtain alteration class informations
+    odict_alts = vcf_reader.alts
+    df_alts_meta = pd.DataFrame(odict_alts, index=('id', 'description')).T.reset_index(drop=True)
+
+    # obtain info field information
+    odict_infos = vcf_reader.infos
+    df_infos_meta = pd.DataFrame(odict_infos, index=('id', 'number', 'type', 'description', 'source', 'version')).T.reset_index(drop=True)
+    ## Delly
+    df_infos_meta.loc[df_infos_meta['id'] == 'SVLEN', 'id'] = 'SVLENORG'
+    df_infos_meta = df_infos_meta.append({'id':'SVLEN', 'number':1, 'type':'Integer', 'description':'Length of SV', 'source':None, 'version':None}, ignore_index=True)
+    print(df_infos_meta)
+    ## /Delly
+
+    # obtain FORMAT informations
+    odict_formats = vcf_reader.formats
+    df_formats_meta = pd.DataFrame(odict_formats, index=('id', 'number', 'type','description')).T.reset_index(drop=True)
+
+    # obtain FILTER informations
+    odict_filters = vcf_reader.filters
+    df_filters_meta = pd.DataFrame(odict_filters, index=('id', 'description')).T.reset_index(drop=True)
+
+    # obtain SAMPLE informations
+    ls_samples = vcf_reader.samples
+    df_samples = pd.DataFrame(ls_samples, columns=['id'])
+
+    odict_df_headers = OrderedDict(
+        contigs_meta = df_contigs_meta,
+        alts_meta = df_alts_meta, 
+        infos_meta = df_infos_meta, 
+        formats_meta = df_formats_meta, 
+        filters_meta = df_filters_meta, 
+        samples_meta = df_samples)
+
+    
+    ls_pos = []
+    ls_filters = []
+    dict_infos = {k: [] for k in df_infos_meta.id}
+    ls_df_formats = []
+
+    
+    for record in vcf_reader:
+        dict_row = {}
+        # ex: Record(CHROM=chr1, POS=9163435, REF=G, ALT=[<DUP:TANDEM>])
+        row_ID = record.ID
+        row_CHROM1 = record.CHROM
+        row_POS1 = record.POS
+        row_REF = record.REF
+        row_ALT = record.ALT[0] # This operation is safe when parsing manta vcf files, but potential cause of information loss in another callers.
+        row_QUAL = record.QUAL
+         
+        
+        #####FILTER
+        ls_filter = record.FILTER
+        if ls_filter is None:
+            ls_filter = ['PASS']
+        elif len(ls_filter) == 0:
+            ls_filter = ['PASS'] 
+        row_FILTER = ls_filter
+        for filter_ in row_FILTER:
+            ls_filters.append({'id': row_ID, 'filter': filter_})
+        #####/FILTER 
+
+        #####INFO
+        row_INFO = record.INFO
+        row_SVTYPE = row_INFO['SVTYPE']
+
+        ##### get svlen
+        if row_SVTYPE == 'BND':
+            svlen = 0
+        elif row_SVTYPE == 'TRA':
+            svlen = 0
+        elif row_SVTYPE == 'DEL':
+            svlen = row_POS1 - row_INFO['END']
+        elif row_SVTYPE == 'DUP':
+            svlen = row_INFO['END'] - row_POS1
+        elif row_SVTYPE == 'INV':
+            svlen = row_INFO['END'] - row_POS1
+        else:
+            svlen = 0
+        ##### /get svlen
+
+        for info in df_infos_meta.id:
+            if info == 'SVLENORG':
+                values = row_INFO.get('SVLEN', 'none')
+            elif info == 'SVLEN':
+                values = svlen
+            else:
+                values = row_INFO.get(info, 'none')
+            if values == 'none':
+                continue
+            if not isinstance(values, list):
+                values = [values]
+            ls_keys = ['id', 'value_idx', info.lower()] 
+            for idx, value in enumerate(values):
+                ls_value = [row_ID, idx, value]
+                dict_a_info = {k: v for k, v in zip(ls_keys, ls_value)}
+                dict_infos[info].append(dict_a_info)
+        
+        #####/INFO
+
+        ###POS
+        if isinstance(row_ALT, vcf.model._Breakend):
+            row_CHROM2 = row_ALT.chr
+            row_POS2 = row_ALT.pos
+            row_STRAND1 = '-' if row_ALT.orientation else '+'
+            row_STRAND2 = '-' if row_ALT.remoteOrientation else '+'
+        elif row_SVTYPE == 'INV': # manta-specific operation
+            row_CHROM2 = row_CHROM1
+            row_POS2 = row_INFO['END']
+            # delly
+            if row_INFO['CT'] == '3to3':
+                row_STRANDs = '+'
+            else:
+                row_POS1 += 1
+                row_POS2 += 1
+                row_STRANDs = '-'
+            # /delly
+            row_STRAND1, row_STRAND2 = row_STRANDs, row_STRANDs
+        elif row_SVTYPE == 'DEL':
+            row_CHROM2 = row_CHROM1
+            row_POS2 = row_INFO['END'] + 1
+            row_STRAND1 = '+'
+            row_STRAND2 = '-'
+        elif row_SVTYPE == 'DUP':
+            row_CHROM2 = row_CHROM1
+            row_POS2 = row_INFO['END']
+            row_POS1 += 1
+            row_STRAND1 = '-'
+            row_STRAND2 = '+'
+        else:
+            row_CHROM2 = row_CHROM1
+            row_POS2 = row_INFO['END']
+            row_STRAND1 = '.'
+            row_STRAND2 = '.'
+
+        ls_pos.append({
+                'id': row_ID, 
+                'chrom1': row_CHROM1, 
+                'pos1': row_POS1, 
+                'chrom2': row_CHROM2, 
+                'pos2': row_POS2, 
+                'strand1': row_STRAND1, 
+                'strand2': row_STRAND2,
+                'ref': row_REF,
+                'alt': row_ALT,
+                'qual': row_QUAL,
+                'svtype': row_SVTYPE
+            })
+        ###/POS
+
+        ####FORMAT
+        format_ = record.FORMAT
+
+        # operation below is limited for manta
+        ls_formats = []
+        for a_sample in record.samples:
+            for a_format in format_.split(':'):
+                values = eval('a_sample.data.' + str(a_format))
+                if not isinstance(values, list):
+                    values = [values]
+                for value_idx in range(len(values)):
+                    ls_formats.append([row_ID, a_sample.sample, a_format, value_idx, values[value_idx]])
+        df_formats_each_record = pd.DataFrame(ls_formats)
+        ls_df_formats.append(df_formats_each_record)
+        # end of the manta-limited operation
+
+        ####/FORMAT
+
+    ###FILTER
+    df_filters = pd.DataFrame(ls_filters)
+    ###/FILTER
+    
+    ###INFO
+    ls_df_infos = []
+    for info in df_infos_meta.id:
+        if len(dict_infos[info]) == 0:
+            df_a_info = pd.DataFrame(columns=('id', 'value_idx', info.lower()))
+        else:
+            df_a_info = pd.DataFrame(dict_infos[info])
+        ls_df_infos.append(df_a_info)
+    odict_df_infos = OrderedDict([(k, v) for k, v in zip(df_infos_meta.id, ls_df_infos)])
+    ###/INFO
+
+    ###POS
+    df_pos = pd.DataFrame(ls_pos)
+    df_pos = df_pos[['id', 'chrom1', 'pos1', 'chrom2', 'pos2', 'strand1', 'strand2', 'ref', 'alt', 'qual', 'svtype']]
+    ###/POS
+
+    ###FORMAT
+    df_formats = pd.concat(ls_df_formats, ignore_index=True)
+    columns = ['id', 'sample', 'format', 'value_idx', 'value']
+    df_formats.columns = columns
+    ###/FORMAT
+   
+    args = [df_pos, df_filters, odict_df_infos, df_formats, odict_df_headers]
+    return Vcf(*args)
+
+################################################
+# /Delly
+################################################
--- src/sgt/io/parser.py
+++ src/sgt/io/parser.py
@@ -12,6 +12,10 @@ from io import StringIO
 from sgt.core.db import Vcf, Bedpe
 from sgt.core.bed import Bed
 from sgt.utils.utils import is_url
+from sgt.io._vcf_parser import (
+    read_vcf_manta,
+    read_vcf_delly,
+)
 pd.set_option('display.max_columns', 10)
 pd.set_option('display.max_colwidth', 30)
 pd.set_option('display.width', 1000) 
@@ -46,6 +50,11 @@ def read_vcf(filepath_or_buffer: Union[str, StringIO], variant_caller: str = "ma
         vcf_reader = vcf.Reader(filepath_or_buffer)
     else:
         raise TypeError("should be file or buffer")
+
+    if variant_caller == 'manta':
+        return read_vcf_manta(vcf_reader)
+    elif variant_caller == 'delly':
+        return read_vcf_delly(vcf_reader)
 
     # obtain header informations
     odict_contigs = vcf_reader.contigs
@@ -237,11 +246,11 @@ def _read_bedpe_empty(df_bedpe):
     ls_header_required = ls_header[:10]
     ls_header_option = ls_header[10:]
     df_svpos = pd.DataFrame(columns=('id', 'chrom1', 'pos1', 'chrom2', 'pos2', 'strand1', 'strand2', 'ref', 'alt', 'qual', 'svtype'))
-    df_svlen = pd.DataFrame(columns=('id', 'svlen_0'))
-    df_svtype = pd.DataFrame(columns=('id', 'svtype_0'))
+    df_svlen = pd.DataFrame(columns=('id', 'value_idx', 'svlen'))
+    df_svtype = pd.DataFrame(columns=('id', 'value_idx', 'svtype'))
     ls_df_infos = []
     for info in ls_header_option:
-        df_info = pd.DataFrame(columns=('id', info + '_0'))
+        df_info = pd.DataFrame(columns=('id', 'value_idx', info))
         ls_df_infos.append(df_info)
     ls_df_infos = [df_svlen, df_svtype] + ls_df_infos   
     ls_infokeys = ['svlen', 'svtype'] + ls_header_option
@@ -316,7 +325,7 @@ def read_bedpe(filepath,
         else:
             x['svlen'] = 0
             return x
-    df_svlen = df_svpos.groupby('svtype').apply(_add_svlen)
+    df_svlen = df_svpos.groupby('svtype').apply(_add_svlen)[['id', 'value_idx', 'svlen']]
 
     ### svtype table
     df_svtype = df_svpos[['id', 'svtype']].copy()

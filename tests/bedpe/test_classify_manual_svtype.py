ede4d7d8e34351f96f78dcbc5b077efbbb7a26df
dermasugita <itsukiisogin@gmail.com>
2021-03-24 10:51:38 +0000

    update tests

--- tests/bedpe/test_classify_manual_svtype.py
+++ tests/bedpe/test_classify_manual_svtype.py
@@ -2,6 +2,7 @@ import os
 import viola
 import pandas as pd
 from io import StringIO
+HERE = os.path.abspath(os.path.dirname(__file__))
 data = """chrom1	start1	end1	chrom2	start2	end2	name	score	strand1	strand2
 chr1	10	11	chr1	20	21	test1	60	+	-
 chr1	10	11	chr1	25	26	test2	60	+	-
@@ -30,6 +31,33 @@ test11	0	tra
 test12	0	tra
 """
 
+DEFINITIONS = """name 'small_del'
+0 SVLEN > -100
+1 SVTYPE == DEL
+logic 0 & 1
+
+name 'large_del'
+0 SVTYPE == DEL
+logic 0
+
+name 'small_dup'
+0 SVLEN < 100
+1 SVTYPE == DUP
+logic 0 & 1
+
+name 'large_dup'
+0 SVTYPE == DUP
+logic 0
+
+name 'small_inv'
+0 SVLEN < 100
+1 SVTYPE == INV
+logic 0 & 1
+
+name 'tra'
+0 SVTYPE == BND
+logic 0
+"""
 
 def small_del(x):
     return x.filter(['svlen > -100', 'svtype == DEL']).ids
@@ -48,7 +76,7 @@ def test_classify_manual_svtype():
     bedpe = viola.read_bedpe(StringIO(data))
     ls_conditions = [small_del, large_del, small_dup, large_dup, small_inv, tra]
     ls_names = ['small_del', 'large_del', 'small_dup', 'large_dup', 'small_inv', 'tra']
-    result = bedpe.classify_manual_svtype(ls_conditions, ls_names)
+    result = bedpe.classify_manual_svtype(ls_conditions=ls_conditions, ls_names=ls_names)
     manual_sv_type = bedpe.manual_sv_type
     manual_sv_type.set_index('id', inplace=True)
     manual_sv_type_expected = pd.read_csv(StringIO(data_expected), sep='\t', names=('id', 'value_idx', 'manual_sv_type'))
@@ -60,5 +88,33 @@ def test_classify_manual_svtype():
     result_expected.name = 'manual_sv_type'
     pd.testing.assert_series_equal(result, result_expected)
     
+def test_classify_manual_svtype_from_definitions():
+    bedpe = viola.read_bedpe(StringIO(data))
+    result = bedpe.classify_manual_svtype(definitions=StringIO(DEFINITIONS))
+    manual_sv_type = bedpe.manual_sv_type
+    manual_sv_type.set_index('id', inplace=True)
+    manual_sv_type_expected = pd.read_csv(StringIO(data_expected), sep='\t', names=('id', 'value_idx', 'manual_sv_type'))
+    manual_sv_type_expected.set_index('id', inplace=True)
+    pd.testing.assert_frame_equal(manual_sv_type, manual_sv_type_expected, check_like=True)
 
+    result_expected = pd.Series([2, 3, 1, 0, 2, 2, 2])
+    ls_names = ['small_del', 'large_del', 'small_dup', 'large_dup', 'small_inv', 'tra']
+    result_expected.index = ls_names + ['others']
+    result_expected.name = 'manual_sv_type'
+    pd.testing.assert_series_equal(result, result_expected)
 
+def test_classify_manual_svtype_from_file():
+    bedpe = viola.read_bedpe(StringIO(data))
+    path = os.path.join(HERE, 'data/example_definition.txt')
+    result = bedpe.classify_manual_svtype(definitions=path)
+    manual_sv_type = bedpe.manual_sv_type
+    manual_sv_type.set_index('id', inplace=True)
+    manual_sv_type_expected = pd.read_csv(StringIO(data_expected), sep='\t', names=('id', 'value_idx', 'manual_sv_type'))
+    manual_sv_type_expected.set_index('id', inplace=True)
+    pd.testing.assert_frame_equal(manual_sv_type, manual_sv_type_expected, check_like=True)
+
+    result_expected = pd.Series([2, 3, 1, 0, 2, 2, 2])
+    ls_names = ['small_del', 'large_del', 'small_dup', 'large_dup', 'small_inv', 'tra']
+    result_expected.index = ls_names + ['others']
+    result_expected.name = 'manual_sv_type'
+    pd.testing.assert_series_equal(result, result_expected)

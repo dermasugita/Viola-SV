68cfc2642e80a07cd550a249dcfa3d87ebb50421
dermasugita <itsukiisogin@gmail.com>
2021-03-24 01:08:35 +0000

    add test file for Vcf.merge

---
+++ tests/vcf/test_merge.py
@@ -0,0 +1,10 @@
+import viola
+import sys, os
+import pandas as pd
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+def test_merge():
+    manta = viola.read_vcf(os.path.join(HERE, 'data/test.merge.manta.vcf'), variant_caller='manta')
+    delly = viola.read_vcf(os.path.join(HERE, 'data/test.merge.delly.vcf'), variant_caller='delly')
+    lumpy = viola.read_vcf(os.path.join(HERE, 'data/test.merge.lumpy.vcf'), variant_caller='lumpy')
+    gridss = viola.read_vcf(os.path.join(HERE, 'data/test.merge.gridss.vcf'), variant_caller='gridss')

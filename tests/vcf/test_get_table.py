b7f3650b1a5e359967100775861b7e5dc5d4b593
dermasugita <itsukiisogin@gmail.com>
2021-03-27 22:19:32 +0000

    resolved conflict

--- tests/vcf/test_get_table.py
+++ tests/vcf/test_get_table.py
@@ -3,17 +3,11 @@ import pytest
 from viola._exceptions import (
     TableNotFoundError,
 )
-<<<<<<< HEAD
 import os
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 def test_get_table():
     data = viola.read_vcf(os.path.join(HERE, 'data/test.manta.vcf'))
-=======
-
-def test_get_table():
-    data = viola.read_vcf('resources/vcf/manta1.vcf')
->>>>>>> 5b88c4e09abac3da91babb308491bb51425a6705
 
     # get positions table
     positions = data.get_table('positions')

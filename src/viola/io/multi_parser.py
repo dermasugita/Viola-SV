a986662af42833769f66e62f0fe4f2988fedf3de
dermasugita <itsukiisogin@gmail.com>
2021-03-28 08:39:55 +0000

    add: exclude option

--- src/viola/io/multi_parser.py
+++ src/viola/io/multi_parser.py
@@ -43,7 +43,8 @@ def read_vcf_multi(dir_path: str,
 
 
 def read_bedpe_multi(dir_path: str,
-    svtype_col_name: str = 'svclass',
+    svtype_col_name: str = None,
+    exclude_empty_cases: bool = False,
     file_extension: str = 'bedpe',
     escape_dot_files: bool = True):
     """
@@ -56,6 +57,8 @@ def read_bedpe_multi(dir_path: str,
         Path to the directory containing the BEDPE files.
     svtype_col_name: str, default 'svclass'
         If the bedpe file has a svtype column, please pass the column name to this argument.
+    exclude_empty_cases: bool, default False
+        If True, skip reading empty BEDPE files.
     file_extension: str or None, default 'bedpe'
         File extension of BEDPE files. If you want to load files with no extension, specify None.
     escape_dot_files: bool, default True
@@ -67,7 +70,9 @@ def read_bedpe_multi(dir_path: str,
         if escape_dot_files and f.startswith('.'): continue
         if (file_extension is not None) and (f.split('.')[-1] != file_extension): continue
         abspath = os.path.abspath(os.path.join(dir_path, f))
-        ls_bedpe.append(read_bedpe(abspath, svtype_col_name=svtype_col_name))
+        bedpe = read_bedpe(abspath, svtype_col_name=svtype_col_name)
+        if exclude_empty_cases & (bedpe.sv_count == 0): continue
+        ls_bedpe.append(bedpe)
         patient_id = f.replace('.bedpe', '')
         ls_names.append(patient_id)
     multi_bedpe = MultiBedpe(ls_bedpe, ls_names)

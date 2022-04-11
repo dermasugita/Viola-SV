c4e7b413d1db5ebcc8e5e7e2c02de145d9c84c98
gpatsugita <itsukiisogin@gmail.com>
2022-04-12 08:27:33 +0900

    add: Vcf.as_bedpe docstrings

--- src/viola/core/vcf.py
+++ src/viola/core/vcf.py
@@ -570,7 +570,36 @@ class Vcf(Bedpe):
         bedpe.to_csv(file_or_buf, index=None, sep='\t')
     
     def as_bedpe(self):
-        df_svpos = self.get_table('posisions')
+        """
+        as_bedpe()
+        Convert Vcf object into Bedpe object.
+
+        Returns
+        --------
+        Bedpe
+
+        Notes
+        ------
+        This process is lossy because only the "positions" table and INFO tables are inherited by Bedpe class.
+
+        Examples
+        ---------
+        >>> import viola
+        >>> vcf = viola.read_vcf('https://raw.githubusercontent.com/dermasugita/ViolaDocs/main/docs/html/_static/tutorial.manta.vcf')
+        >>> bedpe = vcf.as_bedpe()
+        >>> bedpe
+        INFO=imprecise,svtype,svlen,end,cipos,ciend,cigar,mateid,event,homlen,homseq,svinslen,svinsseq,left_svinsseq,right_svinsseq,contig,bnd_depth,mate_bnd_depth,somatic,somaticscore,junction_somaticscore,inv3,inv5
+        Documentation of Bedpe object ==> https://dermasugita.github.io/ViolaDocs/docs/html/reference/bedpe.html
+                id              be1              be2 strand  qual svtype
+        0    test1    chr1:82550461    chr1:82554226     +-  None    DEL
+        1    test2    chr1:22814217    chr1:92581132     --  None    INV
+        2    test3    chr1:60567906    chr1:60675941     +-  None    DEL
+        3    test4    chr1:69583190    chr1:69590948     +-  None    DEL
+        4    test5  chr11:104534877  chr11:104536574     +-  None    DEL
+        5  test6_1  chr11:111134697   chr17:26470495     +-  None    BND
+        6  test6_2   chr17:26470495  chr11:111134697     -+  None    BND
+        """
+        df_svpos = self.get_table('positions')
         odict_df_info = self._odict_df_info
         bedpe = Bedpe(df_svpos, odict_df_info)
         return bedpe

ebfcfae7b2c141b3c072b238cf91d6df7c5b750f
dermasugita <itsukiisogin@gmail.com>
2022-06-06 20:03:05 +0900

    added pon func

--- src/viola/core/vcf.py
+++ src/viola/core/vcf.py
@@ -15,6 +15,7 @@ from viola.core.indexing import Indexer
 from viola.core.bed import Bed
 from viola.core.fasta import Fasta
 from viola.core.bedpe import Bedpe
+#from viola.core.merge import TmpVcfForMerge
 from viola.utils.microhomology import get_microhomology_from_positions
 from viola.utils.utils import get_inslen_and_insseq_from_alt
 from viola._typing import (
@@ -1164,57 +1165,20 @@ class Vcf(Bedpe):
             for j in hit_result:
                 distance_matrix[id_int, j] = 0
                 distance_matrix[j, id_int] = 0
-            print(idx)
-            print(id_int)
             id_int += 1
         return distance_matrix
 
     
-    def merge(self, ls_vcf = [], ls_caller_names = None, mode = 'distance', threshold = 100, linkage = "complete", str_missing = True, integration = True):
-        """
-        merge(ls_vcf:list, ls_caller_names:list, threshold:float, linkage = "complete", str_missing=True, integration=True)
-        Return a merged or integrated vcf object from mulitple  caller's bedpe objects in ls_bedpe
+    def _merge(
+        self,
+        multivcf,
+        ls_caller_names = None,
+        mode = 'distance',
+        threshold = 100,
+        linkage = "complete",
+        str_missing = True,
+        integration = True):
 
-        Parameters
-        ----------
-        ls_vcf:list
-            A list of vcf objects to be merged, which are the same order with ls_caller_names
-        ls_caller_names:list
-            A list of names of bedpe objects to be merged, which should have self's name as the first element
-        mode: {'distance', 'confidence_intervals'}, default 'distance'
-            The mode of the merging strategy. 
-
-            * ``'distance'``: Merge SV records by representative SV positions, that is, coordinates of POS field or that of END in the INFO field. If multiple SV positions are within the distance specified in ``threshold`` each other, they will be merged.
-            * ``'confidence_intervals'``: Merge SV records according to the confidence intervals reported by SV callers. If confidence intervals of multiple SV records share their genomic coordinates at least 1bp, the will be merged.
-
-        threshold:float, default 100
-            Two SVs whose diference of positions is under this threshold are cosidered to be identical.
-            This argument is enabled only when ``mode='distance'``.
-        linkage:{‘complete’, ‘average’, ‘single’}, default ’complete’
-            The linkage of hierarchical clustering.
-            To keep the mutual distance of all SVs in each cluster below the threshold, 
-            "complete" is recommended.
-            This argument is enabled only when ``mode='distance'``.
-        str_missing:bool, default True
-            If True, all the missing strands are considered to be the same with the others.
-        integration:bool, default True 
-            If True, vcf objects in ls_vcf will be merged and integrated with priority as ls_caller_names.
-
-        Returns
-        ----------
-        Vcf
-            A merged  vcf object or an integrated vcf object
-            
-        """
-        if self in ls_vcf:
-            pass
-        else:
-            ls_vcf = [self] + ls_vcf
-        
-        if ls_caller_names is None:
-            ls_caller_names = [vcf._metadata["variantcaller"] for vcf in ls_vcf] 
-
-        multivcf = viola.TmpVcfForMerge(ls_vcf, ls_caller_names)
         if mode == 'distance':
             distance_matrix = self._generate_distance_matrix_by_distance(multivcf, penalty_length=3e9, str_missing=str_missing)
             hcl_clustering_model = AgglomerativeClustering(n_clusters=None, affinity="precomputed", linkage=linkage, distance_threshold=threshold)
@@ -1273,6 +1237,71 @@ class Vcf(Bedpe):
         if integration is True:
             intergrated_vcf = merged_vcf.integrate(merged_vcf = merged_vcf, priority=ls_caller_names)    
             return intergrated_vcf
+    
+    def merge(
+        self,
+        ls_vcf = [],
+        ls_caller_names = None,
+        mode = 'distance',
+        threshold = 100,
+        linkage = "complete",
+        str_missing = True,
+        integration = True):
+        """
+        merge(ls_vcf:list, ls_caller_names:list, threshold:float, linkage = "complete", str_missing=True, integration=True)
+        Return a merged or integrated vcf object from mulitple  caller's bedpe objects in ls_bedpe
+
+        Parameters
+        ----------
+        ls_vcf:list
+            A list of vcf objects to be merged, which are the same order with ls_caller_names
+        ls_caller_names:list
+            A list of names of bedpe objects to be merged, which should have self's name as the first element
+        mode: {'distance', 'confidence_intervals'}, default 'distance'
+            The mode of the merging strategy. 
+
+            * ``'distance'``: Merge SV records by representative SV positions, that is, coordinates of POS field or that of END in the INFO field. If multiple SV positions are within the distance specified in ``threshold`` each other, they will be merged.
+            * ``'confidence_intervals'``: Merge SV records according to the confidence intervals reported by SV callers. If confidence intervals of multiple SV records share their genomic coordinates at least 1bp, the will be merged.
+
+        threshold:float, default 100
+            Two SVs whose diference of positions is under this threshold are cosidered to be identical.
+            This argument is enabled only when ``mode='distance'``.
+        linkage:{‘complete’, ‘average’, ‘single’}, default ’complete’
+            The linkage of hierarchical clustering.
+            To keep the mutual distance of all SVs in each cluster below the threshold, 
+            "complete" is recommended.
+            This argument is enabled only when ``mode='distance'``.
+        str_missing:bool, default True
+            If True, all the missing strands are considered to be the same with the others.
+        integration:bool, default True 
+            If True, vcf objects in ls_vcf will be merged and integrated with priority as ls_caller_names.
+
+        Returns
+        ----------
+        Vcf
+            A merged  vcf object or an integrated vcf object
+            
+        """
+        if self in ls_vcf:
+            pass
+        else:
+            ls_vcf = [self] + ls_vcf
+        
+        if ls_caller_names is None:
+            ls_caller_names = [vcf._metadata["variantcaller"] for vcf in ls_vcf] 
+
+        multivcf = viola.TmpVcfForMerge(ls_vcf, ls_caller_names)
+
+        out = self._merge(
+        multivcf = multivcf,
+        ls_caller_names = ls_caller_names,
+        mode = mode,
+        threshold = threshold,
+        linkage = linkage,
+        str_missing = str_missing,
+        integration = integration)
+
+        return out
 
     def integrate(self, merged_vcf, priority):
         """
---
+++ tests/panel_of_normals/test_create_PON_as_Vcf.py
@@ -0,0 +1,10 @@
+import viola    
+import os
+HERE = os.path.abspath(os.path.dirname(__file__))
+normal1 = viola.read_vcf(os.path.join(HERE, 'data/normal1.vcf'), variant_caller='manta', patient_name='sample1')
+normal2 = viola.read_vcf(os.path.join(HERE, 'data/normal2.vcf'), variant_caller='manta', patient_name='sample2')
+normal3 = viola.read_vcf(os.path.join(HERE, 'data/normal3.vcf'), variant_caller='manta', patient_name='sample3')
+normal4 = viola.read_vcf(os.path.join(HERE, 'data/normal4.vcf'), variant_caller='manta', patient_name='sample4')
+
+def test_create_PON_as_VCF():
+    viola.core.cohort.create_PON_as_Vcf([normal1, normal2, normal3, normal4])

8872f6477b227b81c59b8874a415f8e5c2c84297
dermasugita <itsukiisogin@gmail.com>
2024-04-03 14:09:18 +0000

    accomodated

--- src/viola/domain/vcf/info.py
+++ src/viola/domain/vcf/info.py
@@ -2,26 +2,13 @@ from __future__ import annotations
 from pydantic import BaseModel
 from enum import Enum
 from viola.domain.vcf.data_types import DataType
+from viola.domain.vcf.number import Number
 
-class InfoNumber(BaseModel):
-    value: str
-    ALLOWED_CHARACTERS = "ARG."
-    def __init__(self, value: str):
-        if not isinstance(value, (int, str)) or (isinstance(value, str) and value not in self.ALLOWED_CHARACTERS):
-            message = f"Value must be an integer or one of 'A', 'R', 'G', or '.'"
-            raise ValueError(message)
-        self.value = value
-
-    def __repr__(self):
-        return f"InfoNumber({self.value})"
-    
-    def __str__(self):
-        return self.value
 
 
 class Info(BaseModel):
     _id: str
-    number: InfoNumber
+    number: Number
     type: DataType
     description: str
     source: str | None

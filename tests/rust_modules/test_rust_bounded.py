08353779f080ff829852b7c790177b1a12f10613
dermasugita <itsukiisogin@gmail.com>
2024-07-24 05:24:48 +0000

    added test to ensure read_vcf has been correctly bounded

--- tests/rust_modules/test_rust_bounded.py
+++ tests/rust_modules/test_rust_bounded.py
@@ -1,5 +1,11 @@
 from viola import rs
+from pathlib import Path
+
 
 
 def test_rust_bounded() -> None:
     assert rs.rust_bounded("1") == "1"
+
+def test_read_vcf() -> None:
+    res = rs.read_vcf(Path(__file__).parent / "../../examples/demo_merge/test.merge.manta.vcf")
+    print(res.get_by_id("MD1"))
